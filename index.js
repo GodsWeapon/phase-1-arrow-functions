271678faceced1757ed3b46bad56db54d8c770a4
Trafael Lewis <TrafLewis@gmail.com>
2022-03-27 16:49:57 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+let parameter1 = 2000;
+let parameter2 = 100;
+
+const divide = (parameter1, parameter2) => 2000 / 100;
+
+const square = (nextNumber) => nextNumber * nextNumber;
+
+const add = (finalNumber1, finalNumber2) => finalNumber1 + finalNumber2;
